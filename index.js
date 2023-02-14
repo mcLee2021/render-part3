1064f6ceee4ef31e4ed25e00e2adf91c082ad175
mcLee <mcli2021@zoho.com.cn>
2023-02-14 18:51:31 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,110 @@
+const http = require("http");
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+app.use(cors());
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+morgan.token("data", (req) => {
+  return req.method === "POST" ? JSON.stringify(req.body) : "";
+});
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data")
+);
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+app.get("/info", (request, response) => {
+  response.send(`<p>Phonebook has info for ${persons.length} people</p>
+  <p> ${new Date()}</p>`);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+app.use(unknownEndpoint);
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+  const person = {
+    id: Math.random() * 1000000,
+    name: body.content.name,
+    number: body.content.number,
+  };
+  console.log(person);
+  if (!person.name && !person.number) {
+    return response.status(400).json({
+      error: "name and number missing",
+    });
+  }
+  if (!person.number) {
+    return response.status(400).json({
+      error: "number missing",
+    });
+  }
+  if (!person.name) {
+    return response.status(400).json({
+      error: "name missing",
+    });
+  }
+  if (persons.find((person) => person.name === person.name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  persons = persons.concat(person);
+  response.json(person);
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
